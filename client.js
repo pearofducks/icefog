0447c49f210b43d48c3e02a0127b81026fb3feeb
Dave Honneffer <pearofducks@gmail.com>
2021-03-06 11:20:59 +0100

    Clear data-config after reading

--- client.js
+++ client.js
@@ -2,8 +2,10 @@ export let config = {}
 
 export function initConfig(elementId = 'app', { windowAttr = 'config' } = {}) {
   const appEl = document.getElementById(elementId)
-  if (appEl?.dataset?.config) config = JSON.parse(appEl.dataset.config)
-  else console.warn(`icefog: unable to find data-config on #${elementId}`)
+  if (appEl?.dataset?.config) {
+    config = JSON.parse(appEl.dataset.config)
+    appEl.dataset.config = undefined
+  } else console.warn(`icefog: unable to find data-config on #${elementId}`)
   if (config.isDev) window[windowAttr] = config
 }
 
