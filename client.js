14481ef2be8c7f4630e6fb453ec7111f147dab3b
Dave Honneffer <pearofducks@gmail.com>
2021-03-21 16:36:53 +0100

    Improve compound app behavior

--- client.js
+++ client.js
@@ -1,6 +1,9 @@
 export let config = {}
 
-export function initConfig(elementId = 'app', { windowAttr = 'config' } = {}) {
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+const camelcase = str => str.split('-').map((e, i) => i === 0 ? e.toLowerCase() : capitalize(e)).join('')
+
+export function initConfig(elementId = 'app', { windowAttr = 'configs' } = {}) {
   const appEl = document.getElementById(elementId)
   try {
     config = JSON.parse(appEl.dataset.config)
@@ -8,7 +11,10 @@ export function initConfig(elementId = 'app', { windowAttr = 'config' } = {}) {
   } catch (err) {
     console.warn(`icefog: unable to find data-config on #${elementId}`)
   }
-  if (config.isDev) window[elementId === 'app' ? '' : `${elementId}-` + windowAttr] = config
+  if (config.isDev) {
+    window[windowAttr][camelcase(elementId)] = config
+    if (elementId === 'app') window.config = config
+  }
 }
 
 export const setConfig = c => config = c
