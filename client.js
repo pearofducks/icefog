3983e3efde41cf5731007f05fb99caf80776976f
Dave Honneffer <pearofducks@gmail.com>
2021-03-23 07:21:37 +0100

    Fix

--- client.js
+++ client.js
@@ -4,16 +4,17 @@ const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase
 const camelcase = str => str.split('-').map((e, i) => i === 0 ? e.toLowerCase() : capitalize(e)).join('')
 
 export function initConfig(elementId = 'app', { windowAttr = 'configs' } = {}) {
-  const appEl = document.getElementById(elementId)
   try {
+    const appEl = document.getElementById(elementId)
     config = JSON.parse(appEl.dataset.config)
     appEl.dataset.config = ''
+    if (config.isDev) {
+      if (!window[windowAttr]) window[windowAttr] = {}
+      window[windowAttr][camelcase(elementId)] = config
+      if (elementId === 'app') window.config = config
+    }
   } catch (err) {
     console.warn(`icefog: unable to find data-config on #${elementId}`)
-  }
-  if (config.isDev) {
-    window[windowAttr][camelcase(elementId)] = config
-    if (elementId === 'app') window.config = config
   }
 }
 
