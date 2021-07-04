5cd1cee11d85ac12cd759958e54f5454ec2cbbcc
Dave Honneffer <pearofducks@gmail.com>
2021-07-04 19:15:24 +0200

    More

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ export let config = {}
 const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
 const camelcase = str => str.split('-').map((e, i) => i === 0 ? e.toLowerCase() : capitalize(e)).join('')
 const getConfigEl = (element) => element instanceof HTMLElement ? element : document.querySelector(element)
+const getId = (element) => (element instanceof HTMLElement ? element : document.querySelector(element)).getAttribute('id')
 
 export function getConfig(element) {
   const configEl = getConfigEl(element)
@@ -13,13 +14,14 @@ export function getConfig(element) {
 
 export function initConfig(element = '#app', { windowAttr = 'configs' } = {}) {
   try {
+    const id = getId(element)
     config = getConfig(element)
-    if (config.isDev) {
+    if (config.isDev && id) {
       if (!window[windowAttr]) window[windowAttr] = {}
-      window[windowAttr][camelcase(elementId)] = config
+      window[windowAttr][camelcase(id)] = config
     }
   } catch (err) {
-    console.warn(`icefog: unable to find data-config on ${element}`)
+    console.warn(`Error initializing config: ${err}`)
   }
 }
 
