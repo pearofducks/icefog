82ad6648f5d17be903ebacbf723b455f72866c2f
Dave Honneffer <pearofducks@gmail.com>
2021-07-04 19:39:21 +0200

    More tests

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import serialize from 'serialize-javascript'
 
-export function createConfig(config = {}) {
+export function createConfig(config) {
+  if (typeof config !== 'object') throw new TypeError('config must be an object')
   const json = serialize(config, { isJSON: true, ignoreFunction: true })
   return `data-config='${json}'`
 }
