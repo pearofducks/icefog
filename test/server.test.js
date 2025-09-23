ac3a0ad64b4842ce868c08e0e03a9cc183161406
Dave Honneffer <pearofducks@gmail.com>
2025-09-23 16:06:11 +0200

    Cleanup test

--- test/server.test.js
+++ test/server.test.js
@@ -1,11 +1,7 @@
 import { createConfig } from '../server.js'
-import { suite } from 'uvu'
-import { decode } from '../base64.js'
+import { getConfigFromString } from '../client.js'
 import { describe, it } from 'node:test'
 import * as assert from 'node:assert/strict'
-// import * as assert from 'uvu/assert'
-
-// const Server = suite('Server')
 
 describe('createConfig', () => {
   it('throws if the argument is not an object', () => {
@@ -19,7 +15,18 @@ describe('createConfig', () => {
     const dataAttr = 'data-config='
     assert.ok(result.includes(dataAttr))
     const configString = result.replace(dataAttr, '').replaceAll(`'`, '')
-    const config = JSON.parse(decode(configString))
+    const config = getConfigFromString(configString)
+    assert.equal(config.foo, 'bar')
+    assert.equal(config.negative, false)
+    assert.equal(config.number, 100)
+    assert.equal(config.stringNumber, '200')
+    assert.notEqual(config.stringNumber, 200)
+  })
+
+  it('prepares a DOM-ready string from the object', () => {
+    const obj = { foo: 'bar', negative: false, number: 100, stringNumber: '200' }
+    const configString = createConfig(obj, { raw: true })
+    const config = getConfigFromString(configString)
     assert.equal(config.foo, 'bar')
     assert.equal(config.negative, false)
     assert.equal(config.number, 100)
